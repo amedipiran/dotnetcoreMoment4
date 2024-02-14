39a3811dedc6e59f531bf346713fbba89501af1b
amedipiran <robertopiranamedi@gmail.com>
2024-02-14 11:28:00 +0100

    uppdaterat meddelanden/felmeddelanden

--- Controller/CategoriesController.cs
+++ Controller/CategoriesController.cs
@@ -54,20 +54,20 @@ public async Task<IActionResult> DeleteCategory(int id)
     var category = await _context.Categories.Include(c => c.Songs).FirstOrDefaultAsync(c => c.Id == id);
     if (category == null)
     {
-        return NotFound();
+        return NotFound(new { message = "Kategorin hittades inte." });
     }
 
     if (category.Songs.Any())
     {
-        // Returnera ett felmeddelande om det finns låtar som tillhör kategorin
-        return BadRequest("Kategorin kan inte tas bort eftersom den har tillhörande låtar.");
+        return BadRequest(new { message = "Kategorin kan inte tas bort eftersom den har tillhörande låtar." });
     }
 
     _context.Categories.Remove(category);
     await _context.SaveChangesAsync();
 
-    return NoContent(); 
+    return Ok(new { message = "Kategorin har tagits bort." });
 }
+
 
     }
 }
